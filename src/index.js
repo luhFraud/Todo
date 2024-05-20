e2265270806c28496b58d65042466a409ab75099
luhFraud <spartida0002@gmail.com>
2024-05-19 23:28:10 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
+import { Project } from "./project";
 import { Task } from "./task";
 
-console.log("Yo");
-
 const task = new Task("Finish Task Objects", "Check if eveything works");
-console.log(task);
-console.log(task.getFormattedDate());
+const task2 = new Task("Date Function for task", "Check how the date format works for task", "2024-05-31");
+const task3 = new Task("This Week Tass", "Should get this week tasks", "2024-05-21");
+const project = new Project("Test Project", [task, task2, task3]);
+
+console.log(project.list);
+console.log(project.getTodaysTasks());
+console.log(project.getThisWeeksTasks());
