53ad1fac2f82cc7103f390ecce17cbf4e8c8b5fa
luhFraud <spartida0002@gmail.com>
2024-05-23 23:27:04 -0700

    Created the task object template that will display the tasks

--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ import { Task } from "./task";
 
 const task = new Task("Finish Task Objects", "Check if eveything works");
 const task2 = new Task("Date Function for task", "Check how the date format works for task", "2024-05-31");
-const task3 = new Task("This Week Tass", "Should get this week tasks", "2024-05-21");
+const task3 = new Task("This Week Tass", "Should get this week tasks", "2024-01-21");
 const project = new Project("Test Project", [task, task2, task3]);
 
-console.log(project.list);
-console.log(project.getTodaysTasks());
-console.log(project.getThisWeeksTasks());
+console.log(task3.getFormattedDate())
