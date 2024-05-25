a15d9496e6479da9222c6425caf87d983c9b5f07
luhFraud <spartida0002@gmail.com>
2024-05-24 23:58:15 -0700

    display modes for main and project section

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import { Project } from "./project";
-import { Task } from "./task";
+import UI from "./ui";
 
-const task = new Task("Finish Task Objects", "Check if eveything works");
-const task2 = new Task("Date Function for task", "Check how the date format works for task", "2024-05-31");
-const task3 = new Task("This Week Tass", "Should get this week tasks", "2024-01-21");
-const project = new Project("Test Project", [task, task2, task3]);
+import { Todo } from "./todo";
 
-console.log(task3.getFormattedDate())
+const todo = new Todo();
+
+console.log(todo.projects);
+
+//document.addEventListener("DOMContentLoaded", UI.loadPage)
